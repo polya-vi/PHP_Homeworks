85cf8aa984ade4ad782d83b899021e709fb870fd
Polya Ivanova <polia_vi@abv.bg>
2012-03-31 13:11:12 +0300

    main file homework1

---
+++ homework1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+ <meta name="author" content="Polya Ivanova" />
+	<title>Homework 1</title>
+</head>
+
+<body>
+
+<form action="file.php" method="get">
+			<label for = "var"> Enter a number:</label>
+			<input type = "text" name = "variable" />
+			<input type  = "submit" value = "Ok" />
+
+</body>
+
+</html>
